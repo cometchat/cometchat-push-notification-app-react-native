d9d984091ed9058022d5b10c882d9e61eacdf185
Ritik.CometChat <ritik.kaushik@cometchat.com>
2023-11-15 12:57:49 +0530

    minor fixes

--- src/CONSTS.ts
+++ src/CONSTS.ts
@@ -1,7 +1,7 @@
 export const COMETCHAT_CONSTANTS = {
-  APP_ID: "242842d6698d0f64",
-  REGION: "eu",
-  AUTH_KEY: "41fcbbce045980030a964226943b69844865ca7a",
+  APP_ID: "****************",
+  REGION: "**",
+  AUTH_KEY: "****************************************",
 };
 
 export const SCREENS_CONSTANTS = {
